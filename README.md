846eced723d31eff67c6bcbc41a88a74ca15edc8
mvkno77 <mvkno77@gmail.com>
2017-01-04 02:54:42 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# judo-heroes
+A simple application built to showcase Universal JavaScript with Node.js and React.
+
+
+## License
+Licensed under [MIT License](LICENSE).
---
+++ src/components/Layout.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class Layout extends React.Component {
+  render() {
+    return (
+      <div className="app-container">
+        <header>
+          <Link to="/">
+            <img className="logo" src="/img/logo-judo-heroes.png"/>
+          </Link>
+        </header>
+        <div className="app-content">{this.props.children}</div>
+
+
+      </div>
+    );
+  }
+}
